4b0e3317e3b57c5b3d9c37b7ab1cb659d7858b37
caiselvass <caiselvassala@gmail.com>
2024-03-12 15:58:09 +0100

    ADD file to fix dataset

---
+++ fix_dataset_a_mitges.R
@@ -0,0 +1,12 @@
+load('ultra_updated_data.RData')
+
+data$is_group <- data$Is_Group
+data$gender <- data$Gender
+data$city <- data$City
+data$nationality <- data$Nationality
+
+data <- subset(data, -c(Is_Group, Gender, City, Nationality))
+data$city[data$city == ''] <- NA
+data$nationality[data$nationality == 'England' || data$nationality == 'Scotland', 'nationality'] <- 'United Kingdom'
+data[which(is.na(data$city) && data$nationality == 'United Kingdom')] <- 'London'
+
