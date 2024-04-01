d38dbae13026845627dad094b32b3ff43c7bba7a
pauhidalgoo <pauhidalgopujol@gmail.com>
2024-04-01 12:56:25 +0200

    FIX knn imputed unknown

--- 1_Dataset creation/addunknown.R
+++ 1_Dataset creation/addunknown.R
@@ -2,7 +2,10 @@ load("./3_Preprocessing/data_knn_imputed.RData")
 
 data$is_group <- as.factor(data$is_group)
 
-for(col in names(data)) {
+contains_any_na = sapply(data, function(x) any(is.na(x)))
+nanames <- names(data)[contains_any_na]
+
+for(col in nanames) {
   if(is.factor(data[[col]])) {
     levels(data[[col]]) <- c(levels(data[[col]]), "Unknown")
   }
@@ -11,5 +14,6 @@ for(col in names(data)) {
 data <- replace(data, is.na(data), "Unknown")
 data <- replace(data, data=="", "Unknown")
 summary(data)
+
 
 save(data, file='./3_Preprocessing/data_knn_imputed_unknown.RData')
