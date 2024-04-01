c9c1c53431956972db6d6faf2589114db9337105
pauhidalgoo <pauhidalgopujol@gmail.com>
2024-04-01 12:59:44 +0200

    ADD unique tracks and artists

---
+++ 2_Descriptive analysis/no_repeats.R
@@ -0,0 +1,14 @@
+library(dplyr)
+
+load("./3_Preprocessing/data_knn_imputed_unknown.RData")
+unique_tracks <- data %>%
+  group_by(track_name) %>%
+  slice(1) %>%
+  ungroup()
+
+
+unique_artists <- data %>%
+  group_by(artist_name) %>%
+  slice(1) %>%
+  ungroup()
+
