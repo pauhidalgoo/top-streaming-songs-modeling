de9924ce50383485634fa055da16c68c6997878e
caiselvass <caiselvassala@gmail.com>
2024-04-16 13:55:23 +0200

    MOD shapefiles

--- 7_Geoespacial/geoespacial.R
+++ 7_Geoespacial/geoespacial.R
@@ -1,9 +1,9 @@
 library(sf)
 
-my_sf <- read_sf('./7_Geoespacial/world-administrative-boundaries.shp')
-plot(st_geometry(my_sf), col = "#f2f2f2", bg = "skyblue", lwd = 0.25, border = 0)
+my_sf <- st_read("./7_Geoespacial/world-administrative-boundaries.shp")
+plot(st_geometry(my_sf))
 
 library(ggplot2)
 ggplot(my_sf) +
-  geom_sf(fill = "#69b3a2", color = "white") +
+  geom_sf(fill = "#69b3a2", color = "black") +
   theme_void()
