aaeb12c0ba38a64eea1e841f51cdbc88adb7522f
caiselvass <caiselvassala@gmail.com>
2024-04-23 13:59:33 +0200

    ADD more shape files

--- 7_Geoespacial/geoespacial.R
+++ 7_Geoespacial/geoespacial.R
@@ -1,9 +1,14 @@
 library(sf)
 
-my_sf <- st_read("./7_Geoespacial/world-administrative-boundaries.shp")
+my_sf <- st_read("./7_Geoespacial/ne_10m_populated_places.shp")
 plot(st_geometry(my_sf))
 
 library(ggplot2)
 ggplot(my_sf) +
-  geom_sf(fill = "#69b3a2", color = "black") +
+  geom_sf(fill = "#69b3a2", color = "red") +
+  theme_void()
+
+ggplot(my_sf) +
+  geom_sf(fill = "#69b3a2", color = "red") +  # Draw the points or other geometries
+  geom_sf_text(aes(label = NAME), color = "black", size = 2, nudge_y = 0.1) +  # Add city names
   theme_void()
