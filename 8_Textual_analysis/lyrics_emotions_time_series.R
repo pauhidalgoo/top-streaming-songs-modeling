24277fc448704ad3a0939fdcf43a466504f40280
pauhidalgoo <pauhidalgopujol@gmail.com>
2024-05-16 11:49:40 +0200

    FIX emotions time series plot

--- 8_Textual_analysis/lyrics_emotions_time_series.R
+++ 8_Textual_analysis/lyrics_emotions_time_series.R
@@ -10,23 +10,34 @@ library(textdata) # provides access to lexicon dictionaries
 library(knitr) # used to make kable tables
 
 
-load("./8_textual_analysis/emotions_unique_tracks.RData")
+load("./8_textual_analysis/emotions_unique_translated.RData")
 PATH_PLOTS = paste(getwd(),"./Media/Textual_Analysis",sep="")
 
 emotions_df$release_date <- as.Date(paste(emotions_df$year_release, emotions_df$month_release, "01", sep="-"), format="%Y-%m-%d")
 
-emotion_counts <- aggregate(cbind(anger, anticipation, disgust, fear, joy, sadness, surprise, trust) ~ release_date, data = emotions_df, sum)
+# Filter data to include only years from 2020 onwards
+emotion_counts <- emotion_counts[format(emotion_counts$release_date, "%Y") >= "2017", ]
+
+# Convert counts to percentages
+emotion_counts[, -1] <- emotion_counts[, -1] / rowSums(emotion_counts[, -1]) * 100
 
 # Convert to time series
-emotion_ts <- ts(emotion_counts[, -1], start = c(min(as.integer(as.character(emotions_df$year_release))), 1), frequency = 12)
+emotion_ts <- ts(emotion_counts[, -1], start = c(2017, 1), frequency = 12)
 
-# Plot the time series for each emotion
-plot(emotion_ts[, "anger"], col = "red", type = "l", main = "Emotion Time Series", ylab = "Count", xlab = "Year")
-lines(emotion_ts[, "anticipation"], col = "blue")
-lines(emotion_ts[, "disgust"], col = "green")
-lines(emotion_ts[, "fear"], col = "orange")
-lines(emotion_ts[, "joy"], col = "purple")
-lines(emotion_ts[, "sadness"], col = "brown")
-lines(emotion_ts[, "surprise"], col = "pink")
-lines(emotion_ts[, "trust"], col = "black")
-legend("topright", legend = colnames(emotion_ts), col = c("red", "blue", "green", "orange", "purple", "brown", "pink", "black"), lty = 1)
+# Define line width
+line_width <- 2
+
+# Plot each emotion in a separate plot
+par(mfrow = c(4, 2))  # Setting up a 4x2 grid of plots
+
+plot(emotion_ts[, "anger"], col = "red", type = "l", main = "Anger", ylab = "Percentage", xlab = "Year", lwd = line_width)
+plot(emotion_ts[, "anticipation"], col = "blue", type = "l", main = "Anticipation", ylab = "Percentage", xlab = "Year", lwd = line_width)
+plot(emotion_ts[, "disgust"], col = "green", type = "l", main = "Disgust", ylab = "Percentage", xlab = "Year", lwd = line_width)
+plot(emotion_ts[, "fear"], col = "orange", type = "l", main = "Fear", ylab = "Percentage", xlab = "Year", lwd = line_width)
+plot(emotion_ts[, "joy"], col = "purple", type = "l", main = "Joy", ylab = "Percentage", xlab = "Year", lwd = line_width)
+plot(emotion_ts[, "sadness"], col = "brown", type = "l", main = "Sadness", ylab = "Percentage", xlab = "Year", lwd = line_width)
+plot(emotion_ts[, "surprise"], col = "pink", type = "l", main = "Surprise", ylab = "Percentage", xlab = "Year", lwd = line_width)
+plot(emotion_ts[, "trust"], col = "black", type = "l", main = "Trust", ylab = "Percentage", xlab = "Year", lwd = line_width)
+
+# Reset to default plotting settings
+par(mfrow = c(1, 1))
