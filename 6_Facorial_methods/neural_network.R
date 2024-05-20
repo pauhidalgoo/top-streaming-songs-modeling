79005580606e78e64b3004aced9132b54e618eb0
pauhidalgoo <pauhidalgopujol@gmail.com>
2024-05-20 12:54:38 +0200

    FIX error for merge?

--- 6_Facorial_methods/neural_network.R
+++ 6_Facorial_methods/neural_network.R
@@ -20,6 +20,7 @@ library(dplyr)
 library(caret)
 library(ggplot2)
 library(reshape2)
+library(tidyr)
 
 # ------------------------------------------------------------------------------
 # Prepare the data
@@ -77,6 +78,10 @@ test_data <- as.matrix(test_data)
 # ------------------------------------------------------------------------------
 # Set up and train the model
 
+# Setup early stopping for both models
+early_stop <- callback_early_stopping(monitor = "val_loss", patience = 5, restore_best_weights = TRUE)
+
+# Main model
 model <- keras_model_sequential() %>%
   layer_dense(units = 64, activation = 'relu', input_shape = c(ncol(train_data))) %>%
   layer_dense(units = 64, activation = 'relu') %>%
@@ -88,9 +93,6 @@ model %>% compile(
   metrics = c('accuracy')
 )
 
-# Setup early stopping
-early_stop <- callback_early_stopping(monitor = "val_loss", patience = 10, restore_best_weights = TRUE)
-
 # Train with validation split
 history <- model %>% fit(
   x = train_data, 
@@ -101,83 +103,114 @@ history <- model %>% fit(
   callbacks = list(early_stop)
 )
 
-# ------------------------------------------------------------------------------
-# Plot learning curves
-# Create a data frame from the training history
-history_data <- data.frame(
-  epoch = seq_along(history$metrics$acc),
-  accuracy = history$metrics$acc,
-  val_accuracy = history$metrics$val_acc,
-  loss = history$metrics$loss,
-  val_loss = history$metrics$val_loss
+# Baseline model: Perceptron (Logistic Regression)
+perceptron <- keras_model_sequential() %>%
+  layer_dense(units = 1, activation = 'sigmoid', input_shape = c(ncol(train_data)))
+
+perceptron %>% compile(
+  loss = 'binary_crossentropy',
+  optimizer = 'adam',
+  metrics = c('accuracy')
 )
 
-# Melt the data frame to make it suitable for ggplot
-history_melted <- pivot_longer(history_data, cols = c(accuracy, val_accuracy, loss, val_loss), 
-                               names_to = "metric", values_to = "value")
+# Train with validation split
+history_perceptron <- perceptron %>% fit(
+  x = train_data, 
+  y = train_labels, 
+  epochs = 100, 
+  batch_size = 5,
+  validation_split = 0.2, # Using part of training data as validation data
+  callbacks = list(early_stop)
+)
 
-# Split data into two separate data frames for accuracy and loss for easier plotting
-accuracy_data <- history_melted %>% filter(metric %in% c("accuracy", "val_accuracy"))
-loss_data <- history_melted %>% filter(metric %in% c("loss", "val_loss"))
+# ------------------------------------------------------------------------------
+# Plot learning curves
+plot_training_history <- function(history) {
+  # Create a data frame from the training history
+  history_data <- data.frame(
+    epoch = seq_along(history$metrics$acc),
+    accuracy = history$metrics$acc,
+    val_accuracy = history$metrics$val_acc,
+    loss = history$metrics$loss,
+    val_loss = history$metrics$val_loss
+  )
+  
+  # Melt the data frame to make it suitable for ggplot
+  history_melted <- pivot_longer(history_data, cols = c(accuracy, val_accuracy, loss, val_loss), 
+                                 names_to = "metric", values_to = "value")
+  
+  # Split data into two separate data frames for accuracy and loss for easier plotting
+  accuracy_data <- history_melted %>% filter(metric %in% c("accuracy", "val_accuracy"))
+  loss_data <- history_melted %>% filter(metric %in% c("loss", "val_loss"))
+  
+  # Plotting
+  # Plot for Accuracy
+  p_acc <- ggplot(accuracy_data, aes(x = epoch, y = value, color = metric)) +
+    geom_line(linewidth = 1.25) +
+    labs(title = "Model Accuracy", x = "Epoch", y = "Accuracy") +
+    scale_color_manual(values = c("#1ED760", "#ff7b24"), labels = c("Training Accuracy", "Validation Accuracy")) +
+    ylim(0, 1) +
+    theme_minimal()
+  
+  # Plot for Loss
+  p_loss <- ggplot(loss_data, aes(x = epoch, y = value, color = metric)) +
+    geom_line(linewidth = 1.25) +
+    labs(title = "Model Loss", x = "Epoch", y = "Loss") +
+    scale_color_manual(values = c("#1ED760", "#ff7b24"), labels = c("Training Loss", "Validation Loss")) +
+    ylim(0, 1) +
+    theme_minimal()
+  
+  # Print the plots
+  print(p_acc)
+  print(p_loss)
+}
 
-# Plotting
-# Plot for Accuracy
-p_acc <- ggplot(accuracy_data, aes(x = epoch, y = value, color = metric)) +
-  geom_line(size=1.5) +
-  labs(title = "Model Accuracy", x = "Epoch", y = "Accuracy") +
-  scale_color_manual(values = c("#1ED760", "#ff7b24"), labels = c("Training Accuracy", "Validation Accuracy")) +
-  ylim(0, 1) +
-  theme_minimal()
-
-# Plot for Loss
-p_loss <- ggplot(loss_data, aes(x = epoch, y = value, color = metric)) +
-  geom_line(size=1.5) +
-  labs(title = "Model Loss", x = "Epoch", y = "Loss") +
-  scale_color_manual(values = c("#1ED760", "#ff7b24"), labels = c("Training Loss", "Validation Loss")) +
-  ylim(0, 1) +
-  theme_minimal()
-
-# Print the plots
-print(p_loss)
-print(p_acc)
+plot_training_history(history=history)
+plot_training_history(history=history_perceptron)
 
 # ------------------------------------------------------------------------------
 # Test and evaluation
 
-# Predict probabilities or classes
-predictions_prob <- model %>% predict(test_data)
-predictions <- ifelse(predictions_prob >= 0.5, 1, 0) # Assuming binary classification and threshold of 0.5
+evaluate_model <- function(model, test_data, test_labels, target_name) {
+  # Make predictions
+  predictions_prob <- model %>% predict(test_data)
+  predictions <- ifelse(predictions_prob >= 0.5, 1, 0) # Assuming binary classification and threshold of 0.5
+  
+  # Using caret for a detailed confusion matrix and metrics
+  conf_matrix_caret <- confusionMatrix(as.factor(predictions), as.factor(test_labels), positive = "1")
+  print(conf_matrix_caret)
+  
+  # Extract relevant metrics
+  accuracy <- conf_matrix_caret$overall['Accuracy']
+  precision <- conf_matrix_caret$byClass['Precision']
+  recall <- conf_matrix_caret$byClass['Recall']
+  f1_score <- conf_matrix_caret$byClass['F1']
+  
+  # Print the metrics
+  print(paste("Accuracy:", accuracy))
+  print(paste("Precision:", precision))
+  print(paste("Recall:", recall))
+  print(paste("F1 Score:", f1_score))
+  
+  # Confusion Matrix visualization
+  conf_matrix_table <- as.table(conf_matrix_caret$table)
+  conf_matrix_df <- melt(conf_matrix_table)
+  colnames(conf_matrix_df) <- c("Reference", "Prediction", "Frequency")
+  
+  # Plot the confusion matrix
+  conf_matrix_plot <- ggplot(conf_matrix_df, aes(x = Reference, y = Prediction, fill = Frequency)) +
+    geom_tile() +
+    geom_text(aes(label = Frequency), color = "black", size = 4) +
+    scale_fill_gradient(low = "white", high = "#1ED760") +
+    labs(title = paste("Confusion Matrix for variable", target_name), x = "Actual", y = "Predicted") +
+    scale_x_continuous(breaks = unique(conf_matrix_df$Reference)) +
+    scale_y_continuous(breaks = unique(conf_matrix_df$Prediction)) +
+    theme_minimal()
+  
+  print(conf_matrix_plot)
+}
 
-# Using caret for a detailed confusion matrix and metrics
-conf_matrix_caret <- confusionMatrix(as.factor(predictions), as.factor(test_labels), positive = "1")
-print(conf_matrix_caret)
+evaluate_model(model=model, test_data=test_data, test_labels=test_labels, target_name=target_name)
 
-# Extract relevant metrics
-accuracy <- conf_matrix_caret$overall['Accuracy']
-precision <- conf_matrix_caret$byClass['Precision']
-recall <- conf_matrix_caret$byClass['Recall']
-f1_score <- conf_matrix_caret$byClass['F1']
-
-# Print the metrics
-print(paste("Accuracy:", accuracy))
-print(paste("Precision:", precision))
-print(paste("Recall:", recall))
-print(paste("F1 Score:", f1_score))
-
-# Confusion Matrix visualization
-conf_matrix_table <- as.table(conf_matrix_caret$table)
-conf_matrix_df <- melt(conf_matrix_table)
-colnames(conf_matrix_df) <- c("Reference", "Prediction", "Frequency")
-
-# Plot the confusion matrix
-conf_matrix_plot <- ggplot(conf_matrix_df, aes(x = Reference, y = Prediction, fill = Frequency)) +
-  geom_tile() +
-  geom_text(aes(label = Frequency), color = "black", size = 4) +
-  scale_fill_gradient(low = "white", high = "#1ED760") +
-  labs(title = paste("Confusion Matrix for variable", target_name), x = "Actual", y = "Predicted") +
-  scale_x_continuous(breaks = unique(conf_matrix_df$Reference)) +
-  scale_y_continuous(breaks = unique(conf_matrix_df$Prediction)) +
-  theme_minimal()
-
-print(conf_matrix_plot)
+evaluate_model(model=perceptron, test_data=test_data, test_labels=test_labels, target_name=target_name)
 
--- 8_Textual_analysis/PredictSongFromSentence.R
+++ 8_Textual_analysis/PredictSongFromSentence.R
@@ -18,7 +18,7 @@ clean_corpus <- function(corpus) {
 }
 
 # Function to ensure minimum length of phrase
-extend_phrase <- function(phrase, min_length = 500) {
+extend_phrase <- function(phrase, min_length = 1500) {
   while (nchar(phrase) < min_length) {
     phrase <- paste(phrase, phrase)
   }
@@ -59,11 +59,83 @@ perform_lsa <- function(phrase, n = 5) {
   # Get top n most similar documents
   top_n <- order(similarities, decreasing = TRUE)[1:n]
   similar_docs <- track_names[top_n]
-  
-  return(similar_docs)
+  similar_ids <- unique_tracks$track_id[top_n]
+  llista <- list("documents" = similar_docs, "ids" = similar_ids) 
+  return(llista)
 }
 
 # Example usage
-phrase <- "Me gusta el sexo mami culi fuck yeah"
-top_similar_docs <- perform_lsa(phrase, n = 1)
+phrase1 <- "Me gustas mucho pero la relación no puede seguir"
+phrase2 <- "Baila conmigo toda la noche, bajo la luna llena y las estrellas brillantes."
+phrase3 <- "Late at night, the streets are alive with the fucking pulse of the city. 
+            The bass hits hard as hell, and the beat is relentless, pounding like my
+            heartbeat. I'm out here, grinding, spitting raw-ass bars that tell my
+            fucked-up story. Fuck the haters, fuck the bullshit, I'm rising above all
+            that shit. The struggle is fucking real, but so is my motherfucking hustle. 
+            Every verse is a big fuck you to the doubters, every rhyme a testament to my
+            grind. This ain't just music, it's a goddamn way of life. In the concrete jungle,
+            you either make it or you fucking don't. No bullshit, no excuses, just raw, unfiltered
+            truth. I'm here to claim my spot, and ain't nobody gonna fucking stop me. 
+            The mic is my weapon, and every word is fucking loaded. This is hip hop, 
+            motherfucker. It's gritty, it's real, it's raw as fuck, and it's unapologetically me. 
+            This is my fucking anthem, my story told in rhymes and beats, a middle finger 
+            to the world, a declaration of my fucking existence. Every beat is a battle, 
+            every rhyme a war cry. This is hip hop, and I'm here to fucking conquer it."
+
+
+print(phrase1)
+top_similar_docs <- perform_lsa(phrase1, n=30)
+top_similar_ids1 <- top_similar_docs$ids
+top_similar_docs <- top_similar_docs$documents
 print(top_similar_docs)
+
+print(phrase2)
+top_similar_docs <- perform_lsa(phrase2, n = 30)
+top_similar_ids2 <- top_similar_docs$ids
+top_similar_docs <- top_similar_docs$documents
+print(top_similar_docs)
+
+print(phrase3)
+top_similar_docs <- perform_lsa(phrase3, n = 30)
+top_similar_ids3 <- top_similar_docs$ids
+top_similar_docs <- top_similar_docs$documents
+print(top_similar_docs)
+
+
+# TO SPOTIFY PLAYLIST USING API -------------------------------
+
+library("spotifyr")
+
+
+# Sys.setenv(SPOTIFY_CLIENT_ID = 'XXXXXXXXXXXXXXXX')
+# Sys.setenv(SPOTIFY_CLIENT_SECRET = 'XXXXXXXXXXXXXXXX')
+
+
+create_new_playlist <- function(track_ids, playlist_name) {
+  # Authenticate with Spotify API
+  my_token <- spotifyr::get_spotify_authorization_code(client_id = Sys.getenv("SPOTIFY_CLIENT_ID"),
+                                                   client_secret = Sys.getenv("SPOTIFY_CLIENT_SECRET"),
+                                                   scope = 'playlist-modify-public playlist-read-private')
+  my_token
+  # Create a new playlist
+  new_playlist <- create_playlist(user_id = "31kej347oroci5uq32dpg6wpnpae",
+                                  name = playlist_name,
+                                  public = TRUE,
+                                  collaborative = FALSE,
+                                  description = "Playlist created with LSA for PMAAD",
+                                  authorization  = my_token)
+  
+  # Add tracks to the playlist
+  add_tracks_to_playlist(playlist_id = new_playlist$id,
+                         uris = track_ids,
+                         position = NULL,
+                         authorization = my_token)
+  
+  cat("Playlist", playlist_name, "created successfully!\n")
+}
+create_new_playlist(top_similar_ids1, "Dejarnos de querer")
+
+create_new_playlist(top_similar_ids2, "Bailando de noche")
+
+create_new_playlist(top_similar_ids3, "Hip hop life")
+
