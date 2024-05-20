17c487aa38b3b48d18dc52d99ec8070522e9d47f
pauhidalgoo <pauhidalgopujol@gmail.com>
2024-05-20 11:37:50 +0200

    ADD wordlcloud app fix

--- GUI/wordcloudapp.R
+++ GUI/wordcloudapp.R
@@ -1,2 +1,14 @@
 library(shiny)
-runApp("my_app")
+source("./GUI/global.R")
+source("./GUI/predict_playlist.R")
+
+# Define UI
+source("./GUI/ui.R")
+
+# Define server logic
+source("./GUI/server.R")
+
+plot.new()
+# Run the application
+shinyApp(ui = ui, server = server)
+
