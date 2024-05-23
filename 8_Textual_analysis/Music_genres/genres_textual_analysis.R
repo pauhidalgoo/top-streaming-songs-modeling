6b3f79cd5eda1f609a128f3a0d7bca0b37e2d5fd
pauhidalgoo <pauhidalgopujol@gmail.com>
2024-05-24 01:24:09 +0200

    MOD new genres files ADD topics to dataframe

--- 8_Textual_analysis/Music_genres/genres_textual_analysis.R
+++ 8_Textual_analysis/Music_genres/genres_textual_analysis.R
@@ -2,9 +2,16 @@ library(topicmodels)
 library(ldatuning)
 library(tm)
 library(quanteda)
-
-
+library(ggplot2)
+library(dplyr)
+library(tidytext)
+library(Rtsne)
+library(plotly)
+library(ggrepel)
+library(ggtext)
+# PREPROCESSAMENT DEL TEXT ------------------------------------------------------
 load("./8_Textual_analysis/Music_genres/music_genres_descriptions.RData")
+PATH_PLOTS = paste(getwd(),"./Media/Textual_Analysis/Genres",sep="")
 
 
 uncleaned_corpus<- Corpus(VectorSource(genres_df$content))
@@ -45,7 +52,7 @@ td.mat_freq_10 <- td.mat[terms_freq_10, ]
 
 td.mat <- as.matrix(td.mat_freq_10)
 
-
+# LDA -----------------------------------------------------------------------------
 # Finetunning para escoger el número de topics
 result <- ldatuning::FindTopicsNumber(
   td.mat,
@@ -63,17 +70,12 @@ ap_lda <- LDA(t(td.mat), k = 9, control = list(seed = 42), lowfreq=15)
 ap_lda
 #Word-topic probabilities
 
-library(tidytext)
-
 ap_topics <- tidy(ap_lda, matrix = "beta")
 ap_topics
 
-
-
-library(ggplot2)
-library(dplyr)
-
 dimnames(td.mat)
+
+# VISUALITZACIÓ DELS TOPICS -------------------------------------------------------
 
 ap_top_terms <- ap_topics %>%
   group_by(topic) %>%
@@ -81,6 +83,8 @@ ap_top_terms <- ap_topics %>%
   ungroup() %>%
   arrange(topic, -beta)
 
+png(file=paste0(PATH_PLOTS, "/topic_terms.png"),
+    width=1920, height=1080, units="px", res=130)
 ap_top_terms %>%
   mutate(term = reorder_within(term, beta, topic)) %>%
   ggplot(aes(beta, term, fill = factor(topic))) +
@@ -88,9 +92,8 @@ ap_top_terms %>%
   facet_wrap(~ topic, scales = "free") +
   scale_y_reordered()
 
-library(Rtsne)
-# Extract document-topic distributions from the LDA model
-# Extract document-topic distributions from LDA model
+dev.off()
+
 doc_topic_dist <- posterior(ap_lda)$topics
 
 duplicate_indices <- which(duplicated(doc_topic_dist))
@@ -99,14 +102,9 @@ rownames(doc_topic_dist) <- genres_df$title
 # Remove duplicate rows
 doc_topic_dist <- unique(doc_topic_dist)
 
-
-
 # Perform t-SNE to reduce dimensionality
 tsne_result <- Rtsne(doc_topic_dist)
 
-
-library(ggplot2)
-library(ggrepel)
 tsne_df <- data.frame(tsne_result$Y)
 
 ggplot(tsne_df, aes(x = X1, y = X2)) +
@@ -120,6 +118,8 @@ ggplot(tsne_df, aes(x = X1, y = X2)) +
   xlim(range(tsne_df$X1)) +
   ylim(range(tsne_df$X2))
 
+# CLUSTERING ------------------------------------------------------------------------
+
 
 # Perform K-means clustering
 k <- 8  # Number of clusters, you can adjust this based on your needs
@@ -129,26 +129,20 @@ kmeans_result <- kmeans(doc_topic_dist, centers = k)
 # Get cluster assignments
 cluster_assignments <- kmeans_result$cluster
 
-library(ggplot2)
-library(ggrepel)
-library(ggtext)
-
-library(ggrepel)
-
-# Assuming your tsne_df dataset contains the necessary columns X1, X2, and cluster_assignments
-
 p <- ggplot(tsne_df, aes(x = X1, y = X2, color = factor(cluster_assignments))) +
   geom_point(size = 2, alpha = 0.7) +
   geom_text(aes(label = rownames(doc_topic_dist)),
             size = 3, vjust = 1, hjust = 1) +
   theme_minimal() +
-  labs(title = "t-SNE Visualization of Documents", color = "Topic") +
+  labs(title = "t-SNE Visualization of Music Genres", color = "Cluser") +
   theme(plot.title = element_text(hjust = 0.5)) +
   xlim(range(tsne_df$X1)) +
   ylim(range(tsne_df$X2))
 
+png(file=paste0(PATH_PLOTS, "/clustering_results.png"),
+    width=1920, height=1080, units="px", res=130)
 print(p)
-
+dev.off()
 # Unique cluster codes
 unique_clusters <- unique(cluster_assignments)
 
@@ -163,8 +157,6 @@ cluster_names_assigned <- cluster_names_mapping[cluster_assignments]
 
 cluster_names_assigned <- unname(cluster_names_assigned)
 
-
-
 cluster_doc_df <- data.frame(Cluster = cluster_names_assigned, Document = rownames(doc_topic_dist))
 
 # Create a table showing document names by cluster
@@ -172,12 +164,10 @@ doc_by_cluster <- table(cluster_doc_df$Cluster, cluster_doc_df$Document)
 
 # Print the table
 print(doc_by_cluster)
-
-
-print(p)
-
-library(plotly)
 ggplotly(p)
+
+
+# PLOT DELS TOPICS -----------------------------------------------------------------
 
 
 doc_topic_dist <- posterior(ap_lda)$topics
@@ -205,28 +195,206 @@ p <- ggplot(tsne_df, aes(x = X1, y = X2, color = factor(unname(topics_each)), si
   geom_text(aes(label = rownames(doc_topic_dist)),
             size = 3, vjust = 1, hjust = 1) +
   theme_minimal() +
-  labs(title = "t-SNE Visualization of Documents", color = "Topic") +
+  labs(title = "t-SNE Visualization of Music Genres", color = "Topic") +
   theme(plot.title = element_text(hjust = 0.5)) +
   xlim(range(tsne_df$X1)) +
   ylim(range(tsne_df$X2))
 
+png(file=paste0(PATH_PLOTS, "/topic_results.png"),
+    width=1920, height=1080, units="px", res=130)
 print(p)
+dev.off()
 
-p <- ggplot(tsne_df, aes(x = X1, y = X2, color = factor(unname(topics_each)))) +
-  geom_point(size = 2, alpha = 0.7) +
-  geom_text(aes(label = rownames(doc_topic_dist)),
-            size = 3, vjust = 1, hjust = 1) +
-  theme_minimal() +
-  labs(title = "t-SNE Visualization of Documents", color = "Topic") +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  xlim(range(tsne_df$X1)) +
-  ylim(range(tsne_df$X2))
+unique_topics <- unique(topics_each)
 
-print(p)
+# Names for each cluster code
+topic_names <- c("Electro house", "Reggae Alt", "Regional", "Hip hop", "R&B", "Rock", "Latin & Funk", "Pop", "Folk")
+
+# Create a named vector mapping cluster codes to names
+topic_names_mapping <- setNames(topic_names, unique_topics)
+
+# Assign names to cluster codes
+topic_names_assigned <- topic_names_mapping[topics_each]
+
+topic_names_assigned
+
+topic_names_assigned <- unname(topic_names_assigned)
+
+topic_names_assigned
 
 
-length(unname(topics_each))
-topics_each[1:length(topics_each)]
+topic_doc_df <- data.frame(Topic = topic_names_assigned, Original = rownames(doc_topic_dist))
 
-genres_df$title
+# Create a table showing document names by cluster
+topic_by_cluster <- table(topic_doc_df$Topic, topic_doc_df$Original)
+
+head(topic_doc_df,200)
+
+# PASSEM ELS TOPICS A LES DADES -----------------------------------------------------
+
+
+original_dataset <- read.csv("~/Universitat/4rt Quatri/PMAAD_git/PMAAD_GIA/8_Textual_analysis/Music_genres/spotify-top-200-dataset.csv", sep=";")
+
+load("./8_Textual_analysis/unique_tracks_translated.RData")
+
+merged_data <- unique_translated %>%
+  left_join(distinct(original_dataset, track_id, .keep_all = TRUE), by = "track_id")
+
+definitive_data <- unique_translated
+
+definitive_data$artist_genres <- merged_data$artist_genres
+
+definitive_data$artist_genres
+
+
+definitive_data <- definitive_data %>%
+  mutate(artist_genres = str_split(artist_genres, ", "))
+
+
+unique(definitive_data$artist_genres)
+
+check_genre_match <- function(artist_genres, topic) {
+  for (genre in topic_doc_df$Original[topic_doc_df$Topic == topic]) {
+    for (artist_genre in artist_genres) {
+      artist_genre <- as.character(artist_genre) 
+      if (tolower(genre) == str_to_lower(artist_genre)) {
+        return(TRUE)
+      }
+      if ((str_to_lower(artist_genre) == "r&b") && (tolower(topic) == "r&b")){
+        return(TRUE)
+      }
+      if ((str_to_lower(artist_genre) == "rap") && (tolower(topic) == "hip hop")){
+        return(TRUE)
+      }
+      if ((str_to_lower(artist_genre) == "pop") && (tolower(topic) == "pop")){
+        return(TRUE)
+      }
+      if ((str_to_lower(artist_genre) == "hip hop") && (tolower(topic) == "hip hop")){
+        return(TRUE)
+      }
+      if ((str_to_lower(artist_genre) == "rock") && (tolower(topic) == "rock")){
+        return(TRUE)
+      }
+      if ((str_to_lower(artist_genre) == "edm") && (tolower(topic) == "electro house")){
+        return(TRUE)
+      }
+      if ((str_to_lower(artist_genre) == "house") && (tolower(topic) == "electro house")){
+        return(TRUE)
+      }
+      first_word <- str_split(str_to_lower(artist_genre), " ")[[1]][1]
+      if (first_word == tolower(genre)) {
+        return(TRUE)
+      }
+      if ((first_word == "r&b") && (tolower(topic) == "r&b")){
+        return(TRUE)
+      }
+      if ((first_word == "rap") && (tolower(topic) == "hip hop")){
+        return(TRUE)
+      }
+      if ((first_word == "pop") && (tolower(topic) == "pop")){
+        return(TRUE)
+      }
+      if ((first_word == "rock") && (tolower(topic) == "rock")){
+        return(TRUE)
+      }
+      if ((first_word == "edm") && (tolower(topic) == "electro house")){
+        return(TRUE)
+      }
+      if ((first_word == "house") && (tolower(topic) == "electro house")){
+        return(TRUE)
+      }
+      
+      # Search only the last word of artist_genre
+      last_word <- tail(str_split(str_to_lower(artist_genre), " ")[[1]], 1)
+      if (last_word == tolower(genre)) {
+        return(TRUE)
+      }
+      if ((last_word == "r&b") && (tolower(topic) == "r&b")){
+        return(TRUE)
+      }
+      if ((last_word == "rap") && (tolower(topic) == "hip hop")){
+        return(TRUE)
+      }
+      if ((last_word == "hop") && (tolower(topic) == "hip hop")){
+        return(TRUE)
+      }
+      if ((last_word == "pop") && (tolower(topic) == "pop")){
+        return(TRUE)
+      }
+      if ((last_word == "rock") && (tolower(topic) == "rock")){
+        return(TRUE)
+      }
+      if ((last_word == "edm") && (tolower(topic) == "electro house")){
+        return(TRUE)
+      }
+      if ((last_word == "house") && (tolower(topic) == "electro house")){
+        return(TRUE)
+      }
+    }
+  }
+  return(FALSE)
+}
+
+head(topic_doc_df,140)
+
+# TARDA UNA MICA ÉS NORMAL
+for(topic in unique(topic_doc_df$Topic)) {
+  definitive_data <- definitive_data %>%
+    mutate(!!paste0(topic, "_match") := map_lgl(artist_genres, check_genre_match, topic))
+}
+
+# Ns si fa falta crec que no
+definitive_data[is.na(definitive_data)] <- FALSE
+unique(topic_doc_df$Topic)
+
+# Mirem els que no tenen cap gènere (son només 20)
+no_genre <- definitive_data %>%
+  rowwise() %>%
+  filter(all(across(ends_with("_match"), ~ . == FALSE)))
+
+no_genre
+
+generate_barplot <- function(topic) {
+  topic_data <- definitive_data %>%
+    filter(!!sym(paste0(topic, "_match")))
+  
+  genre_counts <- topic_data %>%
+    unnest(artist_genres) %>%
+    count(artist_genres)
+  
+  # Create the bar plot
+  p <- ggplot(genre_counts, aes(x = artist_genres, y = n)) +
+    geom_bar(stat = "identity", fill = "skyblue") +
+    labs(title = paste("Genres for Topic:", topic),
+         x = "Genre",
+         y = "Frequency") +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  
+  return(p)
+}
+
+plots <- list()
+
+for(topic in unique(topic_doc_df$Topic)) {
+  plots[[topic]] <- generate_barplot(topic)
+}
+
+for(i in seq_along(plots)) {
+  png(file=paste0(PATH_PLOTS, paste0("/", unique(topic_doc_df$Topic)[i],"_subgenres_freq.png")),
+      width=1920, height=1080, units="px", res=130)
+  print(plots[[i]])
+  dev.off()
+}
+
+definitive_data["artist_genres"] <- NULL
+
+load("./8_textual_analysis/emotions_unique_translated.RData")
+
+new_names <- colnames(emotions_df[,49:56])
+
+definitive_data[new_names] <- emotions_df[,49:56]
+
+# Guardem les dades, també amb les emocions
+save(definitive_data, file = "./8_textual_analysis/genres_emotions_translated.RData")
 
--- 8_Textual_analysis/Music_genres/wikipedia_scrapping.R
+++ 8_Textual_analysis/Music_genres/wikipedia_scrapping.R
@@ -9,23 +9,15 @@ genre_list <- strsplit(as.character(original_dataset$artist_genres), ",\\s*")
 # Unlisting the list into a single vector
 all_genres <- unlist(genre_list)
 
-# Removing empty strings (in case there are any)
 all_genres <- all_genres[all_genres != ""]
 
 freq_table <- table(all_genres)
 
 freq_df <- as.data.frame(freq_table)
-
-# Sort the data frame by frequency
 sorted_freq_df <- freq_df[order(-freq_df$Freq), ]
-
-# Print the sorted frequency table
 print(sorted_freq_df)
 
-# Getting the unique genres
 unique_genres <- unique(all_genres)
-
-# Sorting the unique genres (optional)
 unique_genres <- sort(unique_genres)
 unique_genres
 
@@ -34,7 +26,6 @@ url <- "https://en.wikipedia.org/wiki/List_of_music_genres_and_styles"
 # Read the HTML content of the page
 page <- read_html(url)
 
-# Extract the links to individual music genres
 genre_links <- page %>%
   html_nodes("div.div-col li a") %>%
   html_attr("href") %>%
@@ -45,9 +36,7 @@ print(genre_links)
 
 
 extract_genre_name <- function(url) {
-  # Remove the "/wiki/" prefix
   genre <- sub("/wiki/", "", url)
-  # Replace underscores with spaces
   genre <- gsub("_", " ", genre)
   genre <- tolower(genre)
   
@@ -63,8 +52,6 @@ extract_genre_name <- function(url) {
                   "atlanta hip hop" = "atl hip hop",
                   "rhythm and blues" = "r&b",
                   genre) # Default case
-  
-  # Add "rap" to "hip hop" genres
   if (genre == "hip hop music") {
     genre <- c("hip hop", "rap")
   }
@@ -72,36 +59,28 @@ extract_genre_name <- function(url) {
   return(genre)
 }
 
-# Extract genre names from URLs
 extracted_genres <- sapply(genre_links, extract_genre_name)
 
 # Check if the extracted genre names are in the unique genres list
 filtered_urls <- genre_links[extracted_genres %in% unique_genres]
 
-
-# Print the filtered URLs
 print(sort(filtered_urls))
 
 filtered_df <- as.data.frame(sort(filtered_urls))
 
-# Prepend the base URL to get full links
 base_url <- "https://en.wikipedia.org"
 full_links <- paste0(base_url, filtered_urls)
 
 scrape_genre_page <- function(link) {
-  # Read the HTML content of the page
   page <- read_html(link)
   
-  # Extract the title of the page
   title <- page %>% html_node("h1") %>% html_text(trim = TRUE)
   
-  # Extract the main content while excluding irrelevant sections
   content <- page %>% html_node("div.mw-parser-output") %>%
-    html_nodes(xpath = './/p') %>% # Only paragraphs and lists
+    html_nodes(xpath = './/p') %>% # Only paragraphs
     html_text(trim = TRUE) %>%
     paste(collapse = "\n\n") # Combine the text with double line breaks
   
-  # Print done for each page processed
   print("done")
   
   # Return a list with the title and content
@@ -109,14 +88,11 @@ scrape_genre_page <- function(link) {
 }
 
 genres_data <- lapply(full_links, scrape_genre_page)
-
-# Convert the list to a data frame for easier handling
 genres_df <- do.call(rbind, lapply(genres_data, as.data.frame))
 
-
+# ALGUNES PÀGINES ESTAN BUIDES, LES EMPLENEM MANUALMENT ------------------------
 missing_content_indices <- which(is.na(genres_df$content) | genres_df$content == "")
 
-# Iterate over missing content elements and prompt user for input
 for (index in missing_content_indices) {
   cat("Title:", genres_df$title[index], "\n")
   
@@ -125,11 +101,9 @@ for (index in missing_content_indices) {
   content_lines <- character()
   repeat {
     line <- readline(prompt = "")
-    if (line == "") break  # Break the loop if empty line is entered
+    if (line == "") break
     content_lines <- c(content_lines, line)
   }
-  
-  # Combine the content lines into a single string
   content <- paste(content_lines, collapse = "\n")
   genres_df$content[index] <- content
 }
